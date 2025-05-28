eb6cbea2256976f3382084ea4494c3ce29d68797
Cale Conrad <caleconrad23@gmail.com>
2025-05-27 21:18:03 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const clientId = 'f95d2910b2334dd3b2781dd2bff6b329';
+const redirectUri = 'http://127.0.0.1:5500/callback.html';
+
+const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+};
+
+const sha256 = async (plain) => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(plain);
+    return await window.crypto.subtle.digest('SHA-256', data);
+};
+
+const base64encode = (input) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_');
+};
+
+async function startLogin() {
+    const codeVerifier = generateRandomString(64);
+    const hashed = await sha256(codeVerifier);
+    const codeChallenge = base64encode(hashed);
+
+    localStorage.setItem('code_verifier', codeVerifier);
+
+    const scope = 'user-top-read';
+
+    const authUrl = new URL("https://accounts.spotify.com/authorize");
+    const params = {
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge,
+        redirect_uri: redirectUri,
+    };
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
+}
+
+async function getToken(code) {
+    const codeVerifier = localStorage.getItem('code_verifier');
+
+    const body = new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier
+    });
+
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body
+    });
+
+    const data = await response.json();
+    console.log('Token response:', data);
+
+    if (data.access_token) {
+        localStorage.setItem('access_token', data.access_token);
+        window.location.href='index.html';
+    } else {
+        document.body.innerHTML = "Token fetch failed: " + JSON.stringify(data);
+    }
+}
